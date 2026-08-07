03dbfc9509a3c9e403519156452c1eb0b970d03c
codegrann <renderer721@gmail.com>
2026-08-07 14:45:55 +0300

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -56,10 +56,10 @@ export const Header: React.FC<HeaderProps> = ({
 	    alt="The New Covenant Church of Africa - Alpha and Omega"
 	    className="h- w-18 sm:h- sm:w-32 object-contain "
 	  />*/}
-	  <div className=" flex flex-col items-center border-1 border-red-300">
-		  <img src={TopText} alt="The New Covenant Church of Africa" className="w-64 sm:w-72" />
+	  <div className="relative md:top-[-28px] flex flex-col items-center border-1 border-red-300">
+		  <img src={TopText} alt="The New Covenant Church of Africa" className="absolute top-8 w-64 sm:w-72" />
 		  <img src={CathedralIcon} alt="" className="w-16 sm:w-20 my-1" />
-		  <img src={BottomText} alt="Alpha and Omega" className="w-40 sm:w-48" />
+		  <img src={BottomText} alt="Alpha and Omega" className="absolute bottom-3 w-40 sm:w-48" />
 	  </div>
         </Link>
 
